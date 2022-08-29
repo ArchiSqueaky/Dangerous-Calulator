cfdb2fdc231becfc1bd59ac5c09e28cc00cded8e
Wakaism <109924057+ArchiSqueaky@users.noreply.github.com>
2022-08-28 20:50:01 -0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('menu').addEventListener('click', () => {
-    if (document.getElementById('menu').innerHTML === 'menu') {
-        document.getElementById('menu').innerHTML = 'close';
-        document.getElementById('mobiledisplay').style.display = 'flex';
-    } else {
-        document.getElementById('menu').innerHTML = 'menu';
-        document.getElementById('mobiledisplay').style.display = 'none';
-    }
-})
